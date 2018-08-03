957dfc8df295ce1a7b55f9e32549da4661f483cc
mnindrazaka <mnindrazaka@gmail.com>
2018-08-03 15:29:22 +0700

    smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import { Element } from 'react-scroll'
 import 'semantic-ui-css/semantic.min.css'
 
 import Navigation from './components/Navigation'
 import Beranda from './scenes/Beranda'
 
 class App extends Component {
+	menu = ['Beranda', 'Kegiatan', 'Foto', 'Video', 'Tentang Kami', 'Tim Kami']
+
 	render() {
 		return (
 			<div style={styles.container}>
-				<Navigation />
-				<Beranda />
+				<Navigation menu={this.menu} />
+
+				<Element name="Beranda">
+					<Beranda />
+				</Element>
+
+				<Element name="Kegiatan">
+					<Beranda />
+				</Element>
 			</div>
 		)
 	}
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Image } from 'semantic-ui-react'
+import { scroller } from 'react-scroll'
 import styled from 'styled-components'
 
 const Logo = styled(Image)`
@@ -11,25 +12,34 @@ class Navigator extends Component {
 		activeMenu: 'Beranda'
 	}
 
-	menu = ['Beranda', 'Kegiatan', 'Foto', 'Video', 'Tentang Kami', 'Tim Kami']
-
 	renderMenu() {
-		return this.menu.map(item => (
+		return this.props.menu.map(item => (
 			<Menu.Item
 				name={item}
 				link
 				active={this.isMenuActive(item)}
-				onClick={() => this.changeActiveMenu(item)}
+				onClick={() => {
+					this.changeActiveMenu(item)
+					this.scrollTo(item)
+				}}
 			/>
 		))
+	}
+
+	isMenuActive(menu) {
+		return menu === this.state.activeMenu
 	}
 
 	changeActiveMenu(menu) {
 		this.setState({ activeMenu: menu })
 	}
 
-	isMenuActive(menu) {
-		return menu === this.state.activeMenu
+	scrollTo(element) {
+		scroller.scrollTo(element, {
+			duration: 800,
+			delay: 0,
+			smooth: 'easeInOutQuart'
+		})
 	}
 
 	render() {
