623c212ece6014b32c5ea82c1480b95775b134e2
mnindrazaka <mnindrazaka@gmail.com>
2018-12-26 23:21:49 +0700

    add footer link, medium and linkedin

--- src/scenes/Footer/components/DaftarSosmed/index.js
+++ src/scenes/Footer/components/DaftarSosmed/index.js
@@ -6,70 +6,92 @@ class DaftarSosmed extends Component {
   render() {
     return (
       <Fragment>
-        <Grid columns="3" stackable>
+        <Grid columns='3' stackable>
           <Grid.Row>
             <Grid.Column>
-              <a href="https://www.facebook.com/WRIPolinema/">
+              <a href='https://www.facebook.com/groups/wripolinema/'>
                 <SectionItem
-                  title="Facebook"
-                  icon="facebook"
-                  description="WRIPolinema"
-                  color="blue"
-                  size="tiny"
+                  title='Facebook'
+                  icon='facebook'
+                  description='wripolinema'
+                  color='blue'
+                  size='tiny'
                   inverted
                 />
               </a>
               <br />
-              <a href="https://www.instagram.com/wri_polinema/">
+              <a href='https://www.instagram.com/wri_polinema/'>
                 <SectionItem
-                  title="Instagram"
-                  icon="instagram"
-                  description="wri_polinema"
-                  color="pink"
-                  size="tiny"
+                  title='Instagram'
+                  icon='instagram'
+                  description='wri_polinema'
+                  color='pink'
+                  size='tiny'
+                  inverted
+                />
+              </a>
+              <br />
+              <a href='https://www.youtube.com/channel/UCRDuxyMCKmyaX5rRDFo-RCQ'>
+                <SectionItem
+                  title='Youtube'
+                  icon='youtube'
+                  description='Workshop Riset Informatika'
+                  color='red'
+                  size='tiny'
                   inverted
                 />
               </a>
             </Grid.Column>
             <Grid.Column>
-              <a href="https://www.youtube.com/channel/UCRDuxyMCKmyaX5rRDFo-RCQ">
+              <a href='https://github.com/wrideveloper/'>
                 <SectionItem
-                  title="Youtube"
-                  icon="youtube"
-                  description="Workshop Riset Informatika"
-                  color="red"
-                  size="tiny"
+                  title='Github'
+                  icon='github'
+                  description='wrideveloper'
+                  size='tiny'
                   inverted
                 />
               </a>
               <br />
-              <a href="https://github.com/wrideveloper/">
+              <a href='https://medium.com/wripolinema'>
                 <SectionItem
-                  title="Github"
-                  icon="github"
-                  description="wrideveloper"
-                  size="tiny"
+                  title='Medium'
+                  icon='medium'
+                  description='wripolinema'
+                  color='white'
+                  size='tiny'
+                  inverted
+                />
+              </a>
+              <br />
+              <a href='https://www.linkedin.com/company/wripolinema/'>
+                <SectionItem
+                  title='Linkedin'
+                  icon='linkedin'
+                  description='wripolinema'
+                  color='blue'
+                  size='tiny'
                   inverted
                 />
               </a>
             </Grid.Column>
             <Grid.Column>
               <SectionItem
-                title="Email"
-                icon="mail"
-                description="wripolinema@gmail.com"
-                color="orange"
-                size="tiny"
+                title='Email'
+                icon='mail'
+                description='wripolinema@gmail.com'
+                color='orange'
+                size='tiny'
                 inverted
               />
               <br />
-              <a href="https://goo.gl/maps/n3AHzc5yrv92">
+              <a href='https://goo.gl/maps/n3AHzc5yrv92'>
                 <SectionItem
-                  title="Alamat"
-                  icon="map marker alternate"
-                  description="Politeknik Negeri Malang"
-                  color="red"
-                  size="tiny"
+                  title='Alamat'
+                  icon='map marker alternate'
+                  description='Politeknik Negeri Malang'
+                  color='red'
+                  size='tiny'
                   inverted
                 />
               </a>
